58152e9889d47e286d16b6ea558c00de85a2f931
Rev1l00 <111750234+Rev1l00@users.noreply.github.com>
2022-11-11 15:05:40 +0100

    Add files via upload

---
+++ Uke 45 Python/Onsdag-Fredag/pacman/Scripts/powerup.py
@@ -0,0 +1,14 @@
+import pygame
+
+BLACK = (0,0,0)
+
+class PowerUp(pygame.sprite.Sprite):
+    def __init__(self, x, y, filename):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(filename).convert()
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        # Save The image
+        self.powerUp_image = pygame.image.load(filename).convert()
+        self.powerUp_image.set_colorkey(BLACK)
---
+++ Uke 45 Python/Onsdag-Fredag/pacman/Scripts/speed_increase.py
@@ -0,0 +1,3 @@
+from game import *
+
+power_up = powered_up_state
